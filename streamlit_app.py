b6ae9ecda7e8c6b01f80e2827f94b3a0e27603d8
Asad Khan <117889688+asadakkhan@users.noreply.github.com>
2026-03-05 13:47:02 +0530

    Fix URL and parse JSON response for smoothie API

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ if ingredient_list:
         st.success('Your Smoothie is ordered! '+name_on_order, icon="✅")
 
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json()
 
 
 
